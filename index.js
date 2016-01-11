434da8f96454a82050eb95513a5dfbfcf2fdbb63
Roman Salnikov <roman@bardt.me>
2016-01-11 20:01:26 +0600

    Simple page template rendering

--- index.js
+++ index.js
@@ -3,9 +3,12 @@ const express = require('express');
 const app = express();
 
 app.set('port', (process.env.PORT || 5000));
+app.set('view engine', 'ejs')
 
 app.get('/', (req, res) => {
-  res.send('Hello, feature detector!');
+  res.render('demo', function(err, html) {
+    res.send(html);
+  });
 });
 
 app.listen(app.get('port'), () => {
