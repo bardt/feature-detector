8a323dfe7c73ae4fd0bf9d0cdbfe9e1a1fea66fd
Roman Salnikov <roman@bardt.me>
2016-01-15 00:06:13 +0600

    Stats api post method

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const MongoClient = require('mongodb').MongoClient;
+const expressMongoDb = require('express-mongo-db');
 
 const app = express();
 
 app.set('port', (process.env.PORT || 5000));
-app.set('mongoConnectionString', process.env.MONGOLAB_URI);
-app.set('view engine', 'ejs')
+app.set('view engine', 'ejs');
 
 app.use(bodyParser.json());
 app.use(express.static('public'));
+app.use(expressMongoDb(process.env.MONGOLAB_URI || 'mongodb://localhost:27017/'));
 
 app.get('/', (req, res) => {
   res.render('demo', function(err, html) {
@@ -18,8 +18,16 @@ app.get('/', (req, res) => {
 });
 
 app.post('/stats', (req, res) => {
-  MongoClient.connect(app.get('mongoConnectionString'), function(err, db) {
-    db.close();
+  const collection = req.db.collection('statistics');
+  const newRecord = Object.assign({}, req.body, {
+    timestamp: new Date().getTime()
+  });
+  collection.insert(newRecord, (err, result) => {
+    if (err) {
+      res.sendStatus(500);
+    } else {
+      res.sendStatus(200);
+    }
   });
 });
 
