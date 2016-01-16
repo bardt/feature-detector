33151ca4a028310657e6d3adc12a38a04bfdbbaf
Roman Salnikov <roman@bardt.me>
2016-01-16 18:44:33 +0600

    First steps with stats aggregation

--- index.js
+++ index.js
@@ -17,6 +17,39 @@ app.get('/', (req, res) => {
   });
 });
 
+app.get('/stats', (req, res) => {
+  const collection = req.db.collection('statistics');
+  collection.mapReduce(
+    function() {
+      Object.keys(this.features).forEach((key, index) => {
+        if (typeof this.features[key] === 'boolean') {
+          emit(key, this.features[key]);
+        }
+      });
+    },
+    function(key, values) {
+      return values.filter(value => !!value).length / value.length;
+    },
+    {
+      query: { apiKey: 'demo' },
+      out: 'stats_totals'
+    },
+    (err, collecton) => {
+      if (err) {
+        return res.sendStatus(500);
+      }
+
+      collection.find({ stats_totals: true }, (err, result) => {
+        console.log(result);
+        if (err) {
+          return res.sendStatus(500);
+        }
+        res.sendStatus(200);
+      });
+    }
+  );
+});
+
 app.post('/stats', (req, res) => {
   const collection = req.db.collection('statistics');
   const newRecord = Object.assign({}, req.body, {
